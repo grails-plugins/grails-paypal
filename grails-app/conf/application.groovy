8eb12ca66902318cb4be719a721c3acb52fca4ea
mansiarora <mansi@intelligrape.com>
2016-02-04 17:27:26 +0530

    Moved all logic in DataSource.groovy to application.groovy; Deleted DataSource.groovy file;

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,14 +1,47 @@
 environments {
-	production {
-		grails.paypal.server = "https://www.paypal.com/cgi-bin/webscr"
-		grails.paypal.email = "example@business.com"
-		grails.serverURL = "http://www.grails.org"
-	}
-	development {
-		grails.paypal.server = "https://www.sandbox.paypal.com/cgi-bin/webscr"
-		grails.paypal.email = "seller_1237686842_biz@mattstine.com"
-		grails.serverURL = "http://localhost:8080"
-	}
+    production {
+        grails.paypal.server = "https://www.paypal.com/cgi-bin/webscr"
+        grails.paypal.email = "example@business.com"
+        grails.serverURL = "http://www.grails.org"
+    }
+    development {
+        grails.paypal.server = "https://www.sandbox.paypal.com/cgi-bin/webscr"
+        grails.paypal.email = "seller_1237686842_biz@mattstine.com"
+        grails.serverURL = "http://localhost:8080"
+    }
 }
-grails.views.default.codec="none" // none, html, base64
-grails.views.gsp.encoding="UTF-8"
+grails.views.default.codec = "none" // none, html, base64
+grails.views.gsp.encoding = "UTF-8"
+
+dataSource {
+    pooled = true
+    driverClassName = "org.hsqldb.jdbcDriver"
+    username = "sa"
+    password = ""
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.provider_class = 'com.opensymphony.oscache.hibernate.OSCacheProvider'
+}
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+            url = "jdbc:hsqldb:mem:devDB"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:hsqldb:mem:testDb"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+        }
+    }
+}
