4d5e488f3b2740d0ef9e37c5e8a7c5fb714917c0
mansiarora <mansi@intelligrape.com>
2016-02-10 15:33:55 +0530

    Added @ComponentScan annotation in Application.groovy;

--- grails-app/init/grails/paypal/Application.groovy
+++ grails-app/init/grails/paypal/Application.groovy
@@ -2,7 +2,9 @@ package grails.paypal
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
 
+@ComponentScan(basePackages = ['org.grails.plugin.paypal'])
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
